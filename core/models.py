bea36f060f58bfcf3addbd7c8bafe15c7fbb7d3e
Alex Miller <alex.k.miller@gmail.com>
2018-09-25 13:47:43 -0400

    First migration

--- core/models.py
+++ core/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Currency(models.Model):
+    symbol = models.CharField(max_length=10)
+    iso = models.CharField(max_length=3)
+    description = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['iso']
+        verbose_name_plural = "currencies"
+
+    def __str__(self):
+        return self.iso
+
+
+class Organisation(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+class Year(models.Model):
+    value = models.IntegerField()
+
+    def __str__(self):
+        return self.value
+
+
+class SurveyCampaign(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    organisations = models.ManyToManyField(Organisation)
+    years = models.ManyToManyField(Year)
+
+    def __str__(self):
+        return self.title
+
+
+class TableRow(models.Model):
+    value = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.value
+
+
+class TableColumn(models.Model):
+    value = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.value
+
+
+class TableSpecification(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    campaign = models.ForeignKey(SurveyCampaign, on_delete=models.CASCADE)
+    rows = models.ManyToManyField(TableRow)
+    columns = models.ManyToManyField(TableColumn)
+
+    def __str__(self):
+        return self.title
